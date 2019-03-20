21c7663bbb8eecb611080cce918de773ca07e115
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2019-03-20 22:08:03 +0100

    Add simple benchmarks for different optimizations in simple vector operations

--- src/auto-vectorize-example/main.cpp
+++ src/auto-vectorize-example/main.cpp
@@ -4,7 +4,90 @@
 #include <iostream>
 #include <array>
 #include <vector>
+#include <algorithm>
+#include <random>
 #include "../benchmarks/Timer.h"
+
+const int SIZE = 10000;
+
+const double LOWER_BOUND = 0.0;
+const double UPPPER_BOUND = 1000.0;
+
+double Lambda() {
+    std::uniform_real_distribution<double> unif(LOWER_BOUND,UPPPER_BOUND);
+    std::default_random_engine re;
+
+    typedef std::array<double, SIZE> DataArray;
+    DataArray arr_a{};
+    DataArray arr_b{};
+    DataArray arr_res{};
+
+    auto random_double = [&unif, &re]{
+        return unif(re);
+    };
+
+    std::generate(arr_a.begin(), arr_a.end(),random_double);
+    std::generate(arr_b.begin(), arr_b.end(),random_double);
+
+    constexpr double cFixedMultiply = 23.5f;
+
+    auto kernel_multiply = [ &cFixedMultiply ]( DataArray const& a, DataArray const& b, DataArray & res ) -> void  {
+        for( unsigned int i = 0; i < a.size(); ++ i)
+            res[i] = a[i] * b[i] * cFixedMultiply;
+    };
+    kernel_multiply(arr_a, arr_b, arr_res);
+
+    return arr_res[0];
+
+}
+double Baseline() {
+    std::uniform_real_distribution<double> unif(LOWER_BOUND,UPPPER_BOUND);
+    std::default_random_engine re;
+
+    typedef std::vector<double> DataArray;
+    DataArray arr_a(SIZE);
+    DataArray arr_b(SIZE);
+    DataArray arr_res(SIZE);
+
+    auto random_double = [&unif, &re]{
+        return unif(re);
+    };
+
+    std::generate(arr_a.begin(), arr_a.end(),random_double);
+    std::generate(arr_b.begin(), arr_b.end(),random_double);
+
+    double cFixedMultiply = 23.5f;
+
+    for(unsigned int i = 0; i < arr_a.size(); i++) {
+        arr_res[i] = arr_a[i] * arr_b[i] * cFixedMultiply;
+    }
+    return arr_res[0];
+}
+
+double FixedSize() {
+    std::uniform_real_distribution<double> unif(LOWER_BOUND,UPPPER_BOUND);
+    std::default_random_engine re;
+
+    typedef std::array<double, SIZE> DataArray;
+    DataArray arr_a{};
+    DataArray arr_b{};
+    DataArray arr_res{};
+
+    auto random_double = [&unif, &re]{
+        return unif(re);
+    };
+
+    std::generate(arr_a.begin(), arr_a.end(),random_double);
+    std::generate(arr_b.begin(), arr_b.end(),random_double);
+
+    double cFixedMultiply = 23.5f;
+
+    for(unsigned int i = 0; i < arr_a.size(); i++) {
+        arr_res[i] = arr_a[i] * arr_b[i] * cFixedMultiply;
+    }
+
+    return arr_res[0];
+}
 
 
 
@@ -103,10 +186,28 @@ int main() {
     int reps = 1000;
 
     for(int i = 0; i < reps; i++)
-        RunBenchmark();
+        auto result = Baseline();
 
     auto time = timer.GetTime<std::chrono::nanoseconds>();
-    std::cout << "Execution time: " << time.count() / reps;
+    std::cout << "Baseline execution time: " << time.count() / reps;
+
+    timer.Start();
+
+
+    for(int i = 0; i < reps; i++)
+        auto result = FixedSize();
+
+    time = timer.GetTime<std::chrono::nanoseconds>();
+    std::cout << "\nFixedSize execution time: " << time.count() / reps;
+
+    timer.Start();
+
+    for(int i = 0; i < reps; i++)
+        auto result = Lambda();
+
+    time = timer.GetTime<std::chrono::nanoseconds>();
+    std::cout << "\nLambda execution time: " << time.count() / reps;
+
 
     return 0;
 }
--- src/benchmarks/RandomHelper.h
+++ src/benchmarks/RandomHelper.h
@@ -6,16 +6,10 @@
 #include <random>
 class RandomHelper {
 public:
-    static void fill_array(const std::array<> & array);
     static double random_in_range(double from, double to);
 };
 
-void RandomHelper::fill_array(const std::array &array) {
-    for()
-}
-
 double RandomHelper::random_in_range(double from, double to) {
-    std::unifo
     return 0;
 }
 
