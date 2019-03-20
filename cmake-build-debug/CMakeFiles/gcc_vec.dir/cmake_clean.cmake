4b307b99ecaef03ec415193bc1b4c0c385a40cca
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2019-03-20 19:42:14 +0100

    Add Cmake to project. Add information about loop tiling availability to docs

---
+++ cmake-build-debug/CMakeFiles/gcc_vec.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gcc_vec.dir/src/sse-example/rdtsc.s.o"
+  "CMakeFiles/gcc_vec.dir/src/sse-example/main.c.o"
+  "gcc_vec.pdb"
+  "gcc_vec"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/gcc_vec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
