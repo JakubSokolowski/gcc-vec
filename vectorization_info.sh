99fbdce3ad62de2edfca598e6d0e895e8ddc49c2
Jakub Sokołowski <jakub.sokolowski1996@gmail.com>
2019-05-23 19:32:40 +0200

    Minor reformat

--- vectorization_info.sh
+++ vectorization_info.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-#CPU flag detection
+# CPU flag detection
 echo "****Getting CPU flag capabilities and number of cores"
 cat /proc/cpuinfo  | egrep "(flags|model name|vendor)" | sort | uniq -c
-#Compiler capabilities. -march=native is required!
+# Compiler capabilities. -march=native is required!
 echo "****Getting GCC capabilities"
 gcc -march=native -dM -E - < /dev/null | egrep "SSE|AVX" | sort
-#OS kernel version
+# OS kernel version
 echo "****Getting OS Kernel Version"
 uname -a
